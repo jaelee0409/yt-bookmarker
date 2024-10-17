6eeba2ba651f0cf7da05e53e9b124d3d1eaad5a6
U-W10844\CKIRUser <jaeslee@utexas.edu>
2024-10-17 23:45:27 +0900

    Update extension UI

--- ui.js
+++ ui.js
@@ -1,5 +1,25 @@
 import { getCurrentTabURL } from "./utils.js"
 
+const addNewBookmark = () => {
+
+}
+
+const viewBookmarks = (currentBookmarks=[]) => {
+    const bookmarks = document.getElementById("bookmarks");
+    bookmarks.innerHTML = "";
+
+    if (currentBookmarks.length > 0) {
+        for (let i = 0; i < currentBookmarks.length; ++i) {
+            const bookmark = currentBookmarks[i];
+            addNewBookmark(bookmarks, bookmark);
+
+        }
+    }
+    else {
+        bookmarks.innerHTML = '<i class="row">No bookmarks</i>';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const currentTab = await getCurrentTabURL();
     const queryParameters = currentTab.url.split("?")[1];
@@ -12,11 +32,12 @@ document.addEventListener("DOMContentLoaded", async () => {
             const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
 
             // Show bookmarks
+            viewBookmarks(currentVideoBookmarks);
         })
     }
     else {
         // Not a YouTube video url
         const container = document.getElementsByClassName("youtube-bookmark-extension")[0];
-        container.innerHTML = "<div>This is not a YouTube video page.</div>";
+        container.innerHTML = '<div class="title">This is not a YouTube video page.</div>';
     }
 });
